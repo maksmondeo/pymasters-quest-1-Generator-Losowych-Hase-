6c00b360cbdf8990e4c22a5b6bbe9e2daf019ad9
maksmondeo <maks.wazne@gmail.com>
2025-05-19 23:48:16 +0200

    Implement Level Up solution for Quest 1 - include digits and punctuaction in password

--- src/main.py
+++ src/main.py
@@ -1,27 +1,40 @@
 from secrets import choice
-from string import ascii_letters
+from string import ascii_letters, digits, punctuation
 
 
-def password(length):
-    return "".join(choice(ascii_letters) for _ in range(length))
+def password(length: int, include_digits: bool, include_punctuation: bool) -> str:
+    char_pool = ascii_letters
+
+    if include_digits:
+        char_pool += digits
+    if include_punctuation:
+        char_pool += punctuation
+
+    return "".join(choice(char_pool) for _ in range(length))
 
 
-def main():
+def main() -> None:
     length = int(input("\nPodaj długość hasła (liczbę znaków): "))
-    print("\nWygenerowane hasło: " + password(length))
+    include_digits = str(input("\nCzy hasło ma zawierać cyfry? (t/n): "))
+    include_punctuation = str(input("\nCzy hasło ma zawierać znaki specjalne? (t/n): "))
 
-    choice = ""
-    while choice not in ["n", "t"]:
-        choice = input("\nCzy chcesz wygenerować nowe hasło? (t/n): ")
-        if choice == "t":
+    include_digits = include_digits == "t"
+    include_punctuation = include_punctuation == "t"
+
+    print(
+        "\nWygenerowane hasło: " + password(length, include_digits, include_punctuation)
+    )
+
+    new_passoword = ""
+    while new_passoword not in ["n", "t"]:
+        new_passoword = input("\nCzy chcesz wygenerować nowe hasło? (t/n): ")
+        if new_passoword == "t":
             main()
-        elif choice == "n":
+        elif new_passoword == "n":
             print("\nDziękuję za skorzystanie z Generatora Haseł!")
         else:
             print("\nNieprawidłowa odpowiedź.")
 
 
-print(
-    "=== Generator Haseł ===\nWitaj! Ten program pomoże Ci wygenerować bezpieczne, losowe hasło."
-)
+print("Witaj! Ten program pomoże Ci wygenerować bezpieczne, losowe hasło.")
 main()
